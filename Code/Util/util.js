0d347c074661552439f2a0fb55e327b6016d5263
Kyle Nissen <nissenkyle@gmail.com>
2012-03-21 23:44:30 -0600

    Created this module as a way to have consistent logging of messages across the server.  I also tried out JSDoc on it for documentation. Not sure if this is something we should be using but it could help.

---
+++ Code/Util/util.js
@@ -0,0 +1,30 @@
+/**
+ * 	 @fileOverview The class contains several methods (well one just now) that are used to log 
+ * 	 information to different locations (again, just one right now).  
+ *   @author Kyle Nissen
+ *   
+ */
+
+
+/**
+ * @constant LOG_TO_CONSOLE
+ * @description Used for logging information directly to the console
+ */
+this.LOG_TO_CONSOLE =  1;
+
+
+/**
+ * Handles logging messages for the server
+ * @param logCode Code that determines where the information is logged to
+ * @param msg     Message that will be logged to the specified location
+ */
+this.logger = function logger(logCode, msg){
+	switch(logCode)
+	{
+	case this.LOG_TO_CONSOLE:
+		console.log(msg);
+		break;
+	default:
+		console.log(msg);
+	}
+};

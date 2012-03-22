1702387580a11510006f0af82134d14dd9998457
Kyle Nissen <nissenkyle@gmail.com>
2012-03-21 23:42:45 -0600

    Updated some of the functionality.  Just checked that I could properly parse a input path with some search query's built in.  I'll try and build a proper test suite for this to ensure it's working as expected.

--- Code/NodeServer/nodeServer.js
+++ Code/NodeServer/nodeServer.js
@@ -1,10 +1,12 @@
 /**
- * 
+ *  Main module for handling server operations
  */
 
 
 var http = require('http');
 var server = http.createServer();
+
+var util = require('../Util/util.js');
 
 //The path to the root of the server file system
 const ROOT_DIRECTORY = ".";
@@ -17,21 +19,40 @@ const FILE_EXISTS = 409;
 const FILE_IO_ERROR = 500;
 const READ_ERROR = 408;
 
-//Constants for logging options
-const LOG_TO_CONSOLE = 69;
+server.listen(10032);
 
 
 /**
- * Set the server to listen for 'request' events
+ * Sets the server to listen for 'request' events and passes a GET or POST request off
+ * to the correct handler.
  */
 server.on('request', function(request, response){
 	if(request.method == 'GET'){
-		logger(LOG_TO_CONSOLE, new Date() + " -- GET Request recieved: " + request.url);
+		util.logger(util.LOG_TO_CONSOLE, new Date() + " -- GET Request recieved: " + request.url);
 		getHandler(request, response);
 	}
 	else if(request.method == 'POST'){
-		logger(LOG_TO_CONSOLE, new Date() + " -- POST Request recieved: " + request.url);
+		util.logger(util.LOG_TO_CONSOLE, new Date() + " -- POST Request recieved: " + request.url);
 		postHandler(request, response);	
 	}
-});
+});	
+
+/**
+ * Handles all GET requests for the server.
+ */
+function getHandler(request, response){
+	var url = require('url');
+	var urlObj = url.parse(request.url, true);
+	for(var prop in urlObj)
+	{
+		util.logger(util.LOG_TO_CONSOLE, "Property:" + prop + ' ' + urlObj[prop]);
+	}
+ 
+	for(var query in urlObj['query']){
+		util.logger(util.LOG_TO_CONSOLE, "Query:" + query);
+	}
+	
+	response.end();
+}
+
 

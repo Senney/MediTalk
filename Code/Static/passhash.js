794f8fdf27cf47e793f24e6e5e691271b7b5ac27
Sean Heintz <sean.heintz@gmail.com>
2012-04-04 18:56:17 -0600

    Fix layout.

---
+++ Code/Static/passhash.js
@@ -0,0 +1,9 @@
+function submitForm(fieldName, formName) {
+	var pw = document.getElementById(fieldName);
+	if (pw != undefined && pw.value.length == 0) {
+		return;
+	}
+	
+	pw.value = hex_sha256(pw.value);
+	document.getElementById(formName).submit();
+}
